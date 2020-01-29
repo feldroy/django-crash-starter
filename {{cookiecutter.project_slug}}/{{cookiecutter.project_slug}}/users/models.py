3510bcd5358bd372b5ca5b84729f0182375ba01c
Daniel Roy Greenfeld <pydanny@gmail.com>
2020-01-29 13:48:06 -0800

    More cleanup and addition of django-autoslug

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/models.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/models.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import AbstractUser
-from django.db.models import CharField
+from django.db import models
 from django.urls import reverse
 from django.utils.translation import ugettext_lazy as _
 
@@ -8,7 +8,8 @@ class User(AbstractUser):
 
     # First Name and Last Name do not cover name patterns
     # around the globe.
-    name = CharField(_("Name of User"), blank=True, max_length=255)
+    name = models.CharField(_("Name of User"), blank=True, max_length=255)
+    bio = models.TextField("Bio", blank=True)
 
     def get_absolute_url(self):
         return reverse("users:detail", kwargs={"username": self.username})
