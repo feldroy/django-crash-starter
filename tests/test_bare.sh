c66b3989be663c214c55bb7b40c59ca2a0027d1a
Fabio C. Barrioneuvo da Luz <bnafta@gmail.com>
2020-03-10 16:14:07 -0300

    Test with all possible non-windows options

--- tests/test_bare.sh
+++ tests/test_bare.sh
@@ -13,7 +13,7 @@ mkdir -p .cache/bare
 cd .cache/bare
 
 # create the project using the default settings in cookiecutter.json
-cookiecutter ../../ --no-input --overwrite-if-exists
+cookiecutter ../../ --no-input --overwrite-if-exists $@
 cd everycheese
 
 
@@ -21,4 +21,9 @@ cd everycheese
 pip install -r requirements/local.txt
 
 # run the project's tests
-pytest
+echo "Running pytest:"
+pytest || { echo "ERROR: Pytest report some issues"; exit 1; }
+
+echo "Checking missing migrations:"
+# return non-zero status code if there are migrations that have not been created
+python manage.py makemigrations --dry-run --check || { echo "ERROR: there were changes in the models, but migration listed above have not been created and are not saved in version control"; exit 1; }
